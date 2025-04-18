e49783f3c78113ba447df4b1af1806821d0178d4
Firvsama <dblhllw2@gmail.com>
2025-04-18 12:42:55 -0300

    se agregaron comentarios a HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,8 @@
+#se define variable nombre 
 nombre="juan"
+#se define la variable apellido
 apellido="lopez"
+#se define variable materia 
 materia="prog I"
+#se imprime mensaje por consola
 print(f"hola soy {nombre} {apellido} alumno de {materia}")
