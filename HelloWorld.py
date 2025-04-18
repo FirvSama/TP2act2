91727db5733ad4ce01383fec177467ff8c631e24
Firvsama <dblhllw2@gmail.com>
2025-04-18 12:39:38 -0300

    se modifico HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
-nombre="juan"
-apellido="lopez"
-materia="prog I"
+nombre=input("ingrese su nombre: ")
+apellido=input("ingrese su apellido: ")
+materia="Prog I"
 print(f"hola soy {nombre} {apellido} alumno de {materia}")
